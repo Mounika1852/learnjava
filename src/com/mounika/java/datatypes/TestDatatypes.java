3b2e70ce4e7f2a55fa78ebbf9d07c0bce20bce37
Mounika Maddipatla <mounikamaddipatla@Mounikas-MacBook-Pro.local>
2019-06-16 12:49:16 -0400

    added person class

--- src/com/mounika/java/datatypes/TestDatatypes.java
+++ src/com/mounika/java/datatypes/TestDatatypes.java
@@ -1,8 +1,30 @@
 package com.mounika.java.datatypes;
 
 public class TestDatatypes {
+	
 	public static void main(String[] args) {
-		System.out.println("Frist Class");
+		int randomint = 5;
+		long randomlong = 900;
+		char randomchar = 'M';
+		float randomfloat = 4.46f;
+		double randomdouble = 7.8;
+		
+		System.out.println("randomint="+randomint);
+		System.out.println("randomlong="+randomlong);
+		System.out.println("randomchar="+randomchar);
+		System.out.println("randomfloat="+randomfloat);
+		System.out.println("randomdouble="+randomdouble);
+		
+		Person mounika = new Person();
+		mounika.setFirstname("Mounika");
+		mounika.setLastname("Maddipatla");
+		mounika.setAge(24);
+		mounika.setHeight(5.3f);
+		
+		System.out.println(mounika.getFirstname());
+		System.out.println(mounika.getLastname());
+		System.out.println(mounika.getAge());
+		System.out.println(mounika.getHeight());
 	}
 
 }
